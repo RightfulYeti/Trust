c74b889af46c404efa3fe5999e783adaca2df518
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-04 20:41:11 +0100

    Health Bar
    Health Bar

---
+++ Trust Game/Assets/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
