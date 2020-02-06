6ca08de6aa40eb337354815203791b8e26f03d76
Jeppe Willatzen <JeppeWi@outlook.com>
2020-02-06 12:07:09 +0100

    Parralax
    Parralax

---
+++ Trust Game/Assets/Scripts/Parallax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float Length;
+    private float StartPos;
+    public GameObject Camera;
+    public float ParallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position.x;
+        Length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Temp = Camera.transform.position.x * (1 - ParallaxEffect);
+        float Distance = Camera.transform.position.x * ParallaxEffect;
+        transform.position = new Vector3(StartPos + Distance, transform.position.y, transform.position.z);
+
+        if (Temp > StartPos + Length)
+        {
+            StartPos += Length;
+        }
+        else if (Temp < StartPos - Length)
+        {
+            StartPos -= Length;
+        }
+    }
+}
---
+++ Trust Game/Assets/Scripts/UI/HealthBarScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBarScript : MonoBehaviour
+{
+    private const float MAXHEALTH = 100.0f;
+    private Image HealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBar.fillAmount = FindObjectOfType<PlayerScript>().HP / MAXHEALTH;
+    }
+}
